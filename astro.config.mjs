f809433bcd016c24cd0704ab1cafade830e0057b
goeht <tpgoedert@outlook.com>
2024-01-19 18:39:08 +0100

    avancement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import alpine from '@astrojs/alpinejs';
+import postcssNested from 'postcss-nested';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), alpine()],
+  vite: {
+    css: {
+      postcss: {
+        plugins: [
+          postcssNested(),
+        ],
+      },
+    },
+  },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				accent : "#877b9d",
+				accent2 : "#7153ac",
+				secondary: '#FFC857',
+				tertiary: '#C8DBBE',
+				light: '#f0e6df',
+				light2: 'rgba(200,190,210,0.6)',
+				dark: '#5c4a3d',
+			},
+			fontSize: {
+			  'fluid-3xl': 'clamp(1.2rem, 1.5vw, 1.8rem)',
+			},
+			fontFamily: {
+				'serif': ['GEORGIANO','serif'],
+			},
+		},
 	},
 	plugins: [],
 }
